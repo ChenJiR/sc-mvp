0aa7e9cb0eef22242ec61ab25395664f8d0a7ffa
chenjiarui <chenjr2@immatchu.com>
2019-04-30 11:22:05 +0800

    x

---
+++ src/pages/TabFrame.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+
+    <router-view></router-view>
+
+    <tabbar>
+      <tabbar-item selected link="/component/demo" >
+        <img slot="icon" src="../assets/vux_logo.png">
+        <span slot="label">首页</span>
+      </tabbar-item>
+      <tabbar-item show-dot>
+        <img slot="icon" src="../assets/vux_logo.png">
+<!--        <span slot="label">Message</span>-->
+      </tabbar-item>
+      <tabbar-item badge="2">
+        <img slot="icon" src="../assets/vux_logo.png">
+        <span slot="label">我的</span>
+      </tabbar-item>
+    </tabbar>
+  </div>
+</template>
+
+<script>
+  import { Tabbar, TabbarItem } from 'vux'
+
+  export default {
+    components: {
+      Tabbar,
+      TabbarItem
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/index/Index.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <search
+      :results="results"
+      v-model="value"
+      position="absolute"
+      auto-scroll-to-top
+      ref="search"></search>
+
+    <vue-waterfall-easy :imgsArr="imgsArr">
+
+    </vue-waterfall-easy>
+
+  </div>
+</template>
+
+<script>
+  import { Search } from 'vux'
+  import vueWaterfallEasy from 'vue-waterfall-easy'
+
+  export default {
+    components: {
+      Search,
+      vueWaterfallEasy
+    },
+    data() {
+      return {
+        results:[],
+        value:'',
+        imgsArr:[]
+      }
+    },
+    methods:{
+      getData(){
+        this.imgsArr = [
+          {
+            'src':'http://img0.imgtn.bdimg.com/it/u=1398135725,1276761355&fm=26&gp=0.jpg',
+            'info':1,
+            'href':''
+          },
+          {
+            'src':'http://img5.imgtn.bdimg.com/it/u=1289981276,3794620975&fm=26&gp=0.jpg',
+            'info':1,
+            'href':''
+          },
+          {
+            'src':'http://img4.imgtn.bdimg.com/it/u=1535802057,2423234359&fm=15&gp=0.jpg',
+            'info':1,
+            'href':''
+          },
+          {
+            'src':'http://img1.imgtn.bdimg.com/it/u=878709247,2567545902&fm=15&gp=0.jpg',
+            'info':1,
+            'href':''
+          },
+          {
+            'src':'http://img1.imgtn.bdimg.com/it/u=1658773359,884242240&fm=15&gp=0.jpg',
+            'info':1,
+            'href':''
+          },
+        ]
+      }
+    },
+    created() {
+      this.getData()
+    }
+  }
+</script>
+
+<style scoped>
+
+  .item{
+    width: 50% !important;
+  }
+
+</style>
